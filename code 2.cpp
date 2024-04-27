449c61de78b7c8f4fde0cbbe3ff0ecf7c0e3d6db
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2024-04-27 23:55:28 +0530

    displaying patterns using printf function

---
+++ code 2.cpp
@@ -0,0 +1,26 @@
+/*Write a C program to print a block F using the hash (#), 
+where the F has a height of six characters and width of five and four characters.
+ And also print a very large 'C*/
+ #include<stdio.h>
+ int main(){
+ 	printf("Print a block of F:\n");
+	printf("\n# # # # #\n");
+	printf("#\n");
+	printf("# # # #\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	
+    printf("\nPrint a block of C:\n");   
+	printf("\n       # # # #\n");
+	printf("    #\n");
+	printf("  #\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf("  #\n");
+	printf("    #\n");
+	printf("      # # # #\n");
+	return 0;
+	
+	 }
