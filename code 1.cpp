04056cb05d0183c6f2cb50423ecd861c6737cb85
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2024-04-27 23:29:25 +0530

    displaying output using printf function

---
+++ code 1.cpp
@@ -0,0 +1,8 @@
+//WAPdisplaying name , DOB , and mobile number
+#include<stdio.h>
+int main(){
+	printf("Name = ABC\n");
+	printf("DOB = 01-01-1111\n");
+	printf("Mobile number=1234567890\n");
+	return 0;
+	}
