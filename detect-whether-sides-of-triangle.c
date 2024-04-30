9db03249929104af919653fa00a37eeaaedd8428
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-04-30 23:13:46 +0530

    Update detect-whether-sides-of-triangle.c

--- detect-whether-sides-of-triangle.c
+++ detect-whether-sides-of-triangle.c
@@ -7,6 +7,7 @@
     float no1;
     float no2;
     float no3;
+    float perimeter;
 
     printf("Enter the first number = ");
     scanf("%f",&no1);
@@ -20,6 +21,8 @@
     //check whether these are the sides of a triangle or not
     if((no1+no2)>no3 && (no2+no3)>no1 && (no3+no1)>no2){
         printf("\nYes! These are the possible sides of a triangle.");
+        perimeter=no1+no2+no3;
+        printf("\nThe perimeter of the triangle = %f",perimeter);
     }
     else{
         printf("No! These are not the possible sides of a triangle.");
