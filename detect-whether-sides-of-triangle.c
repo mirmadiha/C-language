bfbd5c5da0b1ab59e28f3fa0cae1eebf924a482e
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-04-30 23:09:20 +0530

    Detect whether these are sides of triangle or not.

---
+++ detect-whether-sides-of-triangle.c
@@ -0,0 +1,28 @@
+/*Write a C program that reads three floating-point values,
+ and checks if it is possible to make a triangle with them.
+ Determine the perimeter of the triangle if the given values are valid.*/
+
+ #include<stdio.h>
+ int main(){
+    float no1;
+    float no2;
+    float no3;
+
+    printf("Enter the first number = ");
+    scanf("%f",&no1);
+    
+    printf("Enter the second number = ");
+    scanf("%f",&no2);
+
+    printf("Enter the third number = ");
+    scanf("%f",&no3);
+
+    //check whether these are the sides of a triangle or not
+    if((no1+no2)>no3 && (no2+no3)>no1 && (no3+no1)>no2){
+        printf("\nYes! These are the possible sides of a triangle.");
+    }
+    else{
+        printf("No! These are not the possible sides of a triangle.");
+    }
+    return 0;
+    }
