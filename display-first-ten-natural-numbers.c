e19b0f3bc9ba6bfe16f100e9083817068f95c77a
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-05-05 23:55:08 +0530

    for loop

---
+++ display-first-ten-natural-numbers.c
@@ -0,0 +1,9 @@
+/*Write a program in C to display the first 10 natural numbers*/
+#include<stdio.h>
+int main(){
+    printf("\nThe first ten natural numbers are:");
+    for(int i=1;i<=10;i++){
+        printf("\n%d",i);
+    }
+    return 0;
+}
