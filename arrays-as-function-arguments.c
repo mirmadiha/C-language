0e226ef93e8938551283f599a6bbcb5f7004c052
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-05-15 23:47:39 +0530

    array as function arguments

---
+++ arrays-as-function-arguments.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void printNumb(int arr[],int n);
+
+
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    printNumb(arr,6);
+    return 0;
+}
+
+
+void printNumb(int arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d \t",arr[i]);
+    }
+}
+
+
+
