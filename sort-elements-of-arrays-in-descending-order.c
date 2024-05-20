a4a26866d40fc74f62bba9e882d75f6354bb7a59
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-05-20 23:58:03 +0530

    arrays

---
+++ sort-elements-of-arrays-in-descending-order.c
@@ -0,0 +1,36 @@
+/*Write a program to sort elements of arrays in descending order*/
+#include<stdio.h>
+int main(){
+    int tmp;
+    int n;
+    printf("\nEnter the number of elements in the array = ");
+    scanf("%d",&n);
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("\nEnter the value at index:%d = ",i);
+        scanf("%d",&arr[i]);
+    }
+
+    for(int i=0;i<n;i++){
+        printf("\nThe value at index:%d = %d",i,arr[i]);
+    }
+
+    //sort elements in descending order
+   
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    printf("\nElements of arrays sorted in descending order are:");
+    for(int i=0;i<n;i++){
+        printf("%d",&arr[i]);
+    }
+    printf("\n\n");
+    return 0;
+}
