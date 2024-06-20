823b227e0d8783a6866b801f1bbceff59a3681e4
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-06-21 00:01:27 +0530

    string

---
+++ input-address-using-string.c
@@ -0,0 +1,10 @@
+/*write a program in C to input your address*/
+#include<stdio.h>
+int main()
+{
+        char address[30];
+
+    printf("Enter your address = ");
+    gets(address);
+    return 0;
+}
