361398e4056808c71530e9157f9baea041d83c16
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-06-25 00:00:03 +0530

    Add files via upload

---
+++ count-no-of-vowels-and-consonents.c
@@ -0,0 +1,21 @@
+/*count no. of vowels and consonants in string*/
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int vow=0;
+    int con=0;
+    printf("\nEnter a string= ");
+    gets(str);
+    for(int i=0;i!='\0';i++){
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
+            vow++;
+        }
+        else{
+            con++;
+        }
+    }
+    printf("\nThe number of vowels = %d",vow);
+    printf("\nThe number of consonants = %d",con);
+    return 0;
+}
